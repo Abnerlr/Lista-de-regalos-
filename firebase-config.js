f2e1bffdce944e03ef8388e429197a3bfb59bfed
Abnerlr <abnerlr89@gmail.com>
2026-05-08 11:50:02 -0600

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+ const firebaseConfig = {
+      apiKey: "AIzaSyDL0R2Vi5Na-D-xRff-8RSa766om2FPKbA",
+      authDomain: "lista-de-regalos-de-cocina.firebaseapp.com",
+      databaseURL: "https://lista-de-regalos-de-cocina-default-rtdb.firebaseio.com",
+      projectId: "lista-de-regalos-de-cocina",
+      storageBucket: "lista-de-regalos-de-cocina.firebasestorage.app",
+      messagingSenderId: "8167682777",
+      appId: "1:8167682777:web:64dbc9302b3fc2570084c8",
+      measurementId: "G-1X8V9M6BFZ"
+    };
